995dae19ef6c64e772e4ad0b643b681d49c28f96
Arvind <thakurarvindkr10@gmail.com>
2023-06-07 10:48:16 +0530

    a

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -13,16 +13,16 @@ const Cart = () => {
     <div>
       <h3>Cart</h3>
       <div className="cartWrapper">
-            {
-              products.map((product) => {
-                return <div className="cartCard" key={product.id}>
-                  <img src={product.image} alt="" />
-                  <h5>{product.title}</h5>
-                  <h5>$ {product.price}</h5>
-                  <button className='btn' onClick={() => RemoveProduct(product.id)}>Remove</button>
-                </div>
-              })
-            }
+        {
+          products.map((product) => {
+            return <div className="cartCard" key={product.id}>
+              <img src={product.image} alt="" />
+              <h5>{product.title}</h5>
+              <h5>$ {product.price}</h5>
+              <button className='btn' onClick={() => RemoveProduct(product.id)}>Remove</button>
+            </div>
+          })
+        }
       </div>
     </div>
   )
