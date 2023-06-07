41fe88ca1eb5397cd7912fd24fdd330d3886d180
Arvind <thakurarvindkr10@gmail.com>
2023-06-07 10:47:37 +0530

    product displayed in the cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { remove } from '../Store/cartSlice';
 
 const Cart = () => {
+  const products = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+  console.log(products)
+  const RemoveProduct = (id) => {
+    dispatch(remove(id));
+  }
   return (
     <div>
-      Cart 
+      <h3>Cart</h3>
+      <div className="cartWrapper">
+            {
+              products.map((product) => {
+                return <div className="cartCard" key={product.id}>
+                  <img src={product.image} alt="" />
+                  <h5>{product.title}</h5>
+                  <h5>$ {product.price}</h5>
+                  <button className='btn' onClick={() => RemoveProduct(product.id)}>Remove</button>
+                </div>
+              })
+            }
+      </div>
     </div>
   )
 }
