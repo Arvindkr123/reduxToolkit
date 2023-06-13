ede0d8032108969ff916a6d4145c3bddee0af2e6
Arvind <thakurarvindkr10@gmail.com>
2023-06-13 06:11:14 +0530

    add new dir

---
+++ UserAddRedux/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className="main-div">
+        <div className="child-div">
+          <figure>
+            <figcaption>Add your List here </figcaption>
+          </figure>
+          <div className="addItems">
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
