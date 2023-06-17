ffa84241f61d3473a6087ef1545f4eb1ed4f2606
Arvind <thakurarvindkr10@gmail.com>
2023-06-17 06:57:36 +0530

    I'm project using redux  ecommerce website

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./EcommmerceWithRedux/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
