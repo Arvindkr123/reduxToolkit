2a7333c833df6b1e8c96e40afcc4191f91456e53
Arvind <thakurarvindkr10@gmail.com>
2023-06-17 07:10:41 +0530

    added setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./EcommmerceWithRedux/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      BeVietnamPro: "BeVietnamPro"
+    },
+    extend: {
+      colors: {
+        grey: "#e5e5dc",
+        glass: "rgba(255,255,255,0.45)",
+        transparentBlack: "rgba(0,0,0,0.7)"
+      }
+    },
+    screens: {
+      ss: "480px",
+      sm: "600px",
+      md: "768px",
+      lg: "1099px"
+    },
   },
   plugins: [],
 }
